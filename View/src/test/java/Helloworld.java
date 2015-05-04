fd11727cf2cf2a4fb3f80162746ae4a4790e6f9a
manumontes31 <montes131@gmail.com>
2015-05-04 19:26:55 +0200

    Création d'une interface "NewInter" // Modification de la classe "Helloworld" après avoir implémenté la nouvelle interface

--- View/src/test/java/Helloworld.java
+++ View/src/test/java/Helloworld.java
@@ -1,9 +1,14 @@
 
-public class Helloworld {
+public class Helloworld implements NewInter{
 
 	@Override
 	public String toString() {
 		return "Helloworld []";
+	}
+
+	public void parler() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ View/src/test/java/NewInter.java
@@ -0,0 +1,5 @@
+
+public interface NewInter {
+
+	public void parler();
+}
