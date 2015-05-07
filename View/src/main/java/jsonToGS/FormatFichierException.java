8d3d3047b5672f03b5247f04603a8a714bbde0c3
Hugo Brefel <Brefel.Hugo@gmail.com>
2015-05-07 12:54:28 +0200

    Modification de la classe JsonToGS pour s'adapter au format convenu en réunion

---
+++ View/src/main/java/jsonToGS/FormatFichierException.java
@@ -0,0 +1,28 @@
+package jsonToGS;
+
+@SuppressWarnings("serial")
+public class FormatFichierException extends Exception {
+	
+	//créé une nouvelle instance de TypeFichierException
+	public FormatFichierException(){}
+	
+	/*créé une nouvelle instance de TypeFichierException
+	 * @param message Le message détaillant l'exception */
+	public FormatFichierException(String message){
+		super(message);
+	}
+	
+	/*créé une nouvelle instance de TypeFichierException
+	 * @param cause L'exception à l'origine de cette exception */
+	public FormatFichierException(Throwable cause){
+		super(cause);
+	}
+	
+	/*créé une nouvelle instance de TypeFichierException
+	 * @param message Le message détaillant l'exception
+	 * @param cause L'exception à l'origine de cette exception */
+	public FormatFichierException(String message, Throwable cause){
+		super(message, cause);
+	}
+	
+}
--- View/src/main/java/jsonToGS/JSonToGS.java
+++ View/src/main/java/jsonToGS/JSonToGS.java
@@ -1,99 +1,157 @@
 package jsonToGS;
+
 import java.io.File;
 import java.io.IOException;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class JSonToGS {
 	final static String FILE_PATH = "C:\\Users\\hugo\\Desktop\\jsonTest";
+
+	public static void generateNode(JsonParser jParser, Graph graph) {
+		String name;
+		Node node;
+		try {
+			jParser.nextToken();
+			String fieldname = jParser.getCurrentName();
+			if ("Name".equals(fieldname)) {
+				// current token is "Name",
+				// move to next, which is "Name"'s value
+				jParser.nextToken();
+				name = jParser.getText();
+				node = graph.addNode(name);
+				node.setAttribute("ui.label", name);
+			} else {
+				throw new FormatFichierException(
+						"Le format du fichier sélectionné est invalide");
+			}
+
+			// loop until token equal to "}"
+			while (jParser.nextToken() != JsonToken.END_OBJECT) {
+				fieldname = jParser.getCurrentName();
+				jParser.nextToken();
+				node.addAttribute(fieldname, jParser.getText());
+			}
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (FormatFichierException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	public static void main(String[] args) {
-		Graph graph = new SingleGraph("Test1");
-		String id = null, know = null;
-		Node n = null;
-		
+	public static void generateTransitions(JsonParser jParser, Graph graph){
+		String label = null, noeudD, noeudA;
+		Edge edge;
+		try {
+			jParser.nextToken();
+			String fieldname = jParser.getCurrentName();
+			if ("Label".equals(fieldname)) {
+				// current token is "Label",
+				// move to next, which is "Label"'s value
+				jParser.nextToken();
+				label = jParser.getText();
+			} else {
+				System.out.println(label);
+				throw new FormatFichierException(
+						"Le format du fichier sélectionné est invalide");
+			}
+			jParser.nextToken();
+			fieldname = jParser.getCurrentName();
+			if ("NoeudD".equals(fieldname)) {
+				// current token is "NoeudD",
+				// move to next, which is "NoeudD"'s value
+				jParser.nextToken();
+				noeudD = jParser.getText();
+			} else {
+				throw new FormatFichierException(
+						"Le format du fichier sélectionné est invalide");
+			}
+			jParser.nextToken();
+			fieldname = jParser.getCurrentName();
+			if ("NoeudA".equals(fieldname)) {
+				// current token is "NoeudA",
+				// move to next, which is "NoeudA"'s value
+				jParser.nextToken();
+				noeudA = jParser.getText();
+				edge = graph.addEdge(label, noeudD, noeudA, true);
+				edge.setAttribute("ui.label", label);
+			} else {
+				throw new FormatFichierException(
+						"Le format du fichier sélectionné est invalide");
+			}
+
+			// loop until token equal to "}"
+			while (jParser.nextToken() != JsonToken.END_OBJECT) {
+				fieldname = jParser.getCurrentName();
+				jParser.nextToken();
+				edge.addAttribute(fieldname, jParser.getText());
+			}
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (FormatFichierException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void generateGraph() {
+		Graph graph = new SingleGraph("graph");
+
 		graph.display();
-		
+
 		graph.addAttribute("ui.quality");
 		graph.addAttribute("ui.antialias");
-		graph.addAttribute("ui.stylesheet",
-			"edge { fill-color: grey; }");
+		graph.addAttribute("ui.stylesheet", "edge { fill-color: grey; }");
 
 		try {
 
 			JsonFactory jfactory = new JsonFactory();
 
 			/*** read from file ***/
-			JsonParser jParser = jfactory.createParser(new File(
-					"C:\\Users\\hugo\\Desktop\\jsonTest"));
+			JsonParser jParser = jfactory.createParser(new File(FILE_PATH));
+			jParser.nextToken();
+			jParser.nextToken();
+			String fieldname = jParser.getCurrentName();
 
-			while (jParser.getCurrentToken() != jParser.nextToken()) {
-				// loop until EOF !!! SALE !!! A REFAIRE !!!
-
-				// loop until token equal to "}"
-				while (jParser.nextToken() != JsonToken.END_OBJECT) {
-
-					String fieldname = jParser.getCurrentName();
-					if ("name".equals(fieldname)) {
-
-						// current token is "name",
-						// move to next, which is "name"'s value
-						jParser.nextToken();
-						id = jParser.getText();
-						n = graph.addNode(id);
-						n.setAttribute("ui.label", "nom : " + id);
-
-					}
-
-					if ("age".equals(fieldname)) {
-
-						// current token is "age",
-						// move to next, which is "age"'s value
-						jParser.nextToken();
-						
-						n.addAttribute("age", jParser.getIntValue());
-
-					}
-
-					if ("know".equals(fieldname)) {
-
-						// current token is "know",
-						// move to next, which is "know"'s value
-						jParser.nextToken();
-						know = jParser.getText();
-						if(!know.equals("none")){
-							graph.addEdge(id + "-" + know, id, know, true);
-							graph.getEdge(id + "-" + know).setAttribute("ui.label", id + "-" + know);
-						}
-						
-
-					}
-					
-					if ("knows".equals(fieldname)) {
-						 
-						  jParser.nextToken(); // current token is "[", move next
-				 
-						  // knows is array, loop until token equal to "]"
-						  while (jParser.nextToken() != JsonToken.END_ARRAY) {
-				 
-							know = jParser.getText();
-							graph.addEdge(id + "-" + know, id, know, true);
-							graph.getEdge(id + "-" + know).setAttribute("ui.label", id + "-" + know);
-				 
-						  }
-				 
-						}
-
+			if ("Nodes".equals(fieldname)) {
+				jParser.nextToken(); // current token is "[", move next
+				while (jParser.nextToken() != JsonToken.END_ARRAY) {
+					// nodes is array, loop until token equal to "]"
+					generateNode(jParser, graph);
 				}
+			} else {
+				System.out.println(fieldname);
+				throw new FormatFichierException(
+						"Le format du fichier sélectionné est invalide");
 			}
+
+			jParser.nextToken();
+			fieldname = jParser.getCurrentName();
+
+			if ("Transitions".equals(fieldname)) {
+				jParser.nextToken(); // current token is "[", move next
+				while (jParser.nextToken() != JsonToken.END_ARRAY) {
+					// transitions is array, loop until token equal to "]"
+					generateTransitions(jParser, graph);
+				}
+			} else {
+				throw new FormatFichierException(
+						"Le format du fichier sélectionné est invalide");
+			}
+			
 			jParser.close();
 
 		} catch (JsonGenerationException e) {
@@ -108,8 +166,16 @@ public class JSonToGS {
 
 			e.printStackTrace();
 
+		} catch (FormatFichierException e) {
+
+			e.printStackTrace();
+
 		}
 
+	}
+
+	public static void main(String[] args) {
+		generateGraph();
 	}
 
 }
