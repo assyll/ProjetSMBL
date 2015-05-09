566c5ee63fdc3c159601527bc9019198fc1f4c46
Hugo Brefel <Brefel.Hugo@gmail.com>
2015-05-09 17:18:40 +0200

    Modification du test de generation de Json (complexification)

--- View/src/test/java/jsonAndGSTest/MyJsonGeneratorTest.java
+++ View/src/test/java/jsonAndGSTest/MyJsonGeneratorTest.java
@@ -27,7 +27,13 @@ public void testGenerateJson() {
 		MyJsonNode n3 = new MyJsonNode("node 3", new LinkedList<String>());
 		n3.get_attributs().add("att 1");
 		n3.get_attributs().add("att 2");
-		mLN.addNode(n1, n2, n3);
+		MyJsonNode n4 = new MyJsonNode("node 4", new LinkedList<String>());
+		MyJsonNode n5 = new MyJsonNode("node 5", new LinkedList<String>());
+		MyJsonNode n6 = new MyJsonNode("node 6", new LinkedList<String>());
+		MyJsonNode n7 = new MyJsonNode("node 7", new LinkedList<String>());
+		MyJsonNode n8 = new MyJsonNode("node 8", new LinkedList<String>());
+		MyJsonNode n9 = new MyJsonNode("node 9", new LinkedList<String>());
+		mLN.addNode(n1, n2, n3, n4, n5, n6, n7, n8, n9);
 
 		MyJsonTransition t1 = new MyJsonTransition("transition 1", "node 1",
 				"node 2", new LinkedList<String>());
@@ -41,7 +47,21 @@ public void testGenerateJson() {
 				"node 3", new LinkedList<String>());
 		t3.get_attributs().add("att 1");
 		t3.get_attributs().add("att 2");
-		mLT.addTransition(t1, t2, t3);
+		MyJsonTransition t4 = new MyJsonTransition("transition 4", "node 1",
+				"node 4", new LinkedList<String>());
+		MyJsonTransition t5 = new MyJsonTransition("transition 5", "node 4",
+				"node 7", new LinkedList<String>());
+		MyJsonTransition t6 = new MyJsonTransition("transition 6", "node 6",
+				"node 5", new LinkedList<String>());
+		MyJsonTransition t7 = new MyJsonTransition("transition 7", "node 5",
+				"node 7", new LinkedList<String>());
+		MyJsonTransition t8 = new MyJsonTransition("transition 8", "node 6",
+				"node 8", new LinkedList<String>());
+		MyJsonTransition t9 = new MyJsonTransition("transition 9", "node 8",
+				"node 9", new LinkedList<String>());
+		MyJsonTransition t10 = new MyJsonTransition("transition 10", "node 9",
+				"node 3", new LinkedList<String>());
+		mLT.addTransition(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10);
 
 		assertEquals(generator.generateJson(mLN, mLT).exists(), true);
 	}
