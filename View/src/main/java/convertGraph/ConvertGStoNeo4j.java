e931ca4eaab8e11e923ce35fb7dde03accf82b67
jak09 <marquette.quentin@orange.fr>
2015-05-18 15:49:38 +0200

    Conversion Neo4J a GraphStream fonctionne !

---
+++ View/src/main/java/convertGraph/ConvertGStoNeo4j.java
@@ -0,0 +1,107 @@
+package convertGraph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+public class ConvertGStoNeo4j {
+	
+	public final static String _pathDestinationNeo4j =
+			"/home/quentin/Documents/Mes_graphes/graphToGS";
+	
+	private final Graph _graphGS;
+	
+	public ConvertGStoNeo4j(Graph graphGS) {
+		_graphGS = graphGS;
+	}
+	
+	public void convertToNeo4j(String pathDestination) {
+		
+		// Correspondance des noeuds graphStream aux noeuds Neo4J
+		Map<org.graphstream.graph.Node, Node> mapNodes = 
+				new HashMap<org.graphstream.graph.Node, Node>();
+		
+		// Creer (ou ouvre) le graphe grace au path.
+		GraphDatabaseService graphNeo4j = new GraphDatabaseFactory().
+				newEmbeddedDatabase(pathDestination);
+		registerShutdownHook(graphNeo4j);
+		
+		try (Transaction tx = graphNeo4j.beginTx()) {
+			
+			// Parcours des noeuds
+			for (org.graphstream.graph.Node nodeGS : _graphGS.getEachNode()) {
+				
+				// Creation du noeud
+				Node nodeNeo4j = graphNeo4j.createNode();
+				
+				// Ajout du label
+				nodeNeo4j.addLabel(DynamicLabel.label(
+						(Boolean.parseBoolean((String)
+								nodeGS.getAttribute("Source")))
+								? "Racine" : "Noeud"));
+				
+				// Ajout des proprietes
+				nodeNeo4j.setProperty("name", nodeGS.getId());
+				for (String attributeKey : nodeGS.getAttributeKeySet()) {
+					nodeNeo4j.setProperty(
+							attributeKey, nodeGS.getAttribute(attributeKey));
+				}
+				
+				// Ajout de la correspondance dans la map
+				mapNodes.put(nodeGS, nodeNeo4j);
+			}
+
+			// Parcours des transitions
+			for (Edge edgeGS : _graphGS.getEachEdge()) {
+				
+				// Recuperation du noeud start
+				Node nodeStart = mapNodes.get(edgeGS.getNode0());
+				
+				// Recuperation du noeud end
+				Node nodeEnd = mapNodes.get(edgeGS.getNode1());
+				
+				// Creation de la transition
+				Relationship relationshipNeo4j = nodeStart.
+						createRelationshipTo(nodeEnd,
+								DynamicRelationshipType.
+								withName("Transition"));
+				
+				// Ajout des proprietes
+				relationshipNeo4j.setProperty("name", edgeGS.getId());
+				for (String attributeKey : edgeGS.getAttributeKeySet()) {
+					relationshipNeo4j.setProperty(
+							attributeKey, edgeGS.getAttribute(attributeKey));
+				}
+			}
+			
+			tx.success();
+		}
+		
+		graphNeo4j.shutdown();
+	}
+	
+	/**
+	 * Fermeture du graphe.
+	 * @param graphDB Graphe a fermer.
+	 */
+	private void registerShutdownHook(
+			final GraphDatabaseService graphDB) {
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				graphDB.shutdown();
+			}
+		});
+	}
+	
+}
---
+++ View/src/main/java/convertGraph/ConvertNeo4jToGS.java
@@ -0,0 +1,104 @@
+package convertGraph;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+public class ConvertNeo4jToGS {
+	
+	private final GraphDatabaseService _graphNeo4j;
+	
+	public ConvertNeo4jToGS(String pathGraphNeo4j) {
+		_graphNeo4j = new GraphDatabaseFactory().
+				newEmbeddedDatabase(pathGraphNeo4j);
+	}
+	
+	public Graph convertToGS() {
+		
+		// Liste des relationship
+		List<Relationship> relationships = new ArrayList<Relationship>();
+		
+		// Graphe "graphStream"
+		Graph graphGS = new MultiGraph("graph2");
+		
+		// Parcours du graphe Neo4j
+		try (Transaction tx = _graphNeo4j.beginTx()) {
+			
+			@SuppressWarnings({ "deprecation", "unused" })
+			Iterator<org.neo4j.graphdb.Node> iteratorNodesNeo4j =
+					_graphNeo4j.getAllNodes().iterator();
+			
+			// Parcours des noeuds
+			while (iteratorNodesNeo4j.hasNext()) {
+				
+				org.neo4j.graphdb.Node nodeNeo4j = iteratorNodesNeo4j.next();
+				
+				// Creation du noeud
+				Node nodeGS = graphGS.addNode(
+						(String) nodeNeo4j.getProperty("name"));
+				
+				// Ajout des attributs
+				Iterator<String> iteratorProperties =
+						nodeNeo4j.getPropertyKeys().iterator();
+				
+				while (iteratorProperties.hasNext()) {
+					String propertyKey = iteratorProperties.next();
+					nodeGS.addAttribute(
+							propertyKey, nodeNeo4j.getProperty(propertyKey));
+				}
+				
+				// Ajout de l'attribut source
+				boolean isSource = nodeNeo4j.
+						getLabels().iterator().next().name().equals("Racine");
+				nodeGS.addAttribute("Source", isSource ? "true" : "false");
+				
+				// Enregistrement des relationship du noeud
+				Iterator<Relationship> iteratorRelationship =
+						nodeNeo4j.getRelationships(Direction.OUTGOING)
+						.iterator();
+				while (iteratorRelationship.hasNext()) {
+					relationships.add(iteratorRelationship.next());
+				}
+			}
+			
+			// Parcours des transitions
+			for (Relationship relationship: relationships) {
+				
+				// Creation de la edge
+				String idEdge = (String) relationship.getProperty("name");
+				String idNodeStart = (String) relationship.
+						getStartNode().getProperty("name");
+				String idNodeEnd = (String) relationship.
+						getEndNode().getProperty("name");
+				Edge edgeGS = graphGS.
+						addEdge(idEdge, idNodeStart, idNodeEnd, true);
+				
+				// Ajout des attributs
+				Iterator<String> iteratorPropertiesKey =
+						relationship.getPropertyKeys().iterator();
+				
+				while (iteratorPropertiesKey.hasNext()) {
+					String propertyKey = iteratorPropertiesKey.next();
+					edgeGS.addAttribute(propertyKey,
+							relationship.getProperty(propertyKey));
+				}
+				
+			}
+			
+			tx.success();
+		}
+		
+		return graphGS;	
+	}
+	
+}
